d2480f094a618232213121698a86b34992470597
Joanna Hsiao <chsiao58@gmail.com>
2019-05-28 17:43:07 -0400

    lab is done

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -7,7 +10,7 @@ public class StringArrayUtils {
     /**
      * @param array array of String objects
      * @return first element of specified array
-     */ // TODO
+     */
     public static String getFirstElement(String[] array) {
         return array[0];
     }
@@ -23,84 +26,149 @@ public class StringArrayUtils {
     /**
      * @param array array of String objects
      * @return last element in specified array
-     */ // TODO
+     */
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1];
     }
 
     /**
      * @param array array of String objects
      * @return second to last element in specified array
-     */ // TODO
+     */
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length-2];
     }
 
     /**
      * @param array array of String objects
      * @param value value to check array for
      * @return true if the array contains the specified `value`
-     */ // TODO
+     */
     public static boolean contains(String[] array, String value) {
+        for(String string: array)
+            if (string.equals(value))
+                return true;
         return false;
     }
 
     /**
      * @param array of String objects
      * @return an array with identical contents in reverse order
-     */ // TODO
+     */
     public static String[] reverse(String[] array) {
-        return null;
+        String[] reversed = new String[array.length];
+
+        for(Integer i = 0; i < array.length; i++)
+            reversed[i] = array[(array.length-1)-i];
+        return reversed;
     }
 
     /**
      * @param array array of String objects
      * @return true if the order of the array is the same backwards and forwards
-     */ // TODO
+     */
     public static boolean isPalindromic(String[] array) {
-        return false;
+        for(Integer i = 0; i < array.length / 2; i++)
+            if (array[i] != array[(array.length-1)-i])
+                return false;
+        return true;
     }
 
     /**
      * @param array array of String objects
      * @return true if each letter in the alphabet has been used in the array
-     */ // TODO
+     */
     public static boolean isPangramic(String[] array) {
-        return false;
+
+        for (Integer index = 0; index < array.length; index++)
+            array[index] = array[index].toUpperCase();
+
+        for(Character alphabet = 'A'; alphabet <= 'Z'; alphabet++) {
+            Boolean isAlphabetIn = false;
+            for (String word : array) {
+                if (word.indexOf(alphabet) != -1) {
+                    isAlphabetIn = true;
+                }
+                if (isAlphabetIn)    // if word contain that alphabet, stop searching this alphabet in all word
+                    break;
+            }
+            if(!isAlphabetIn)   // if there is one alphabet not in this array, it is not Pangramic
+                return false;
+        }
+        return true;
     }
 
     /**
      * @param array array of String objects
      * @param value value to check array for
      * @return number of occurrences the specified `value` has occurred
-     */ // TODO
+     */
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        Integer counter = 0;
+        for(String word: array)
+            if (word.equals(value))
+                counter++;
+
+        return counter;
     }
 
     /**
      * @param array         array of String objects
      * @param valueToRemove value to remove from array
      * @return array with identical contents excluding values of `value`
-     */ // TODO
+     */
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        String[] result = new String[array.length-getNumberOfOccurrences(array,valueToRemove)];
+
+        Integer resultIndex = 0;
+        for(String word: array)
+            if (!word.equals(valueToRemove)) {
+                result[resultIndex] = word;
+                resultIndex++;
+            }
+        return result;
     }
 
     /**
      * @param array array of chars
      * @return array of Strings with consecutive duplicates removes
-     */ // TODO
+     */
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+        List<String> resultList = new ArrayList<String>();
+        resultList.add(array[0]);
+
+        for(Integer index = 1; index < array.length; index++)
+            if(!array[index].equals(array[index-1]))
+                resultList.add(array[index]);
+
+        String[] result = new String[resultList.size()];
+        result = resultList.toArray(result);
+
+        return result;
     }
 
     /**
      * @param array array of chars
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
-     */ // TODO
+     */
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        List<String> resultList = new ArrayList<String>();
+        resultList.add(array[0]);
+
+        for(Integer index = 1; index < array.length; index++)
+            if(!array[index].equals(array[index-1]))    // add the string to result
+                resultList.add(array[index]);
+            else                                        // if it's a repeat, concat to the last element
+                resultList.set(resultList.size()-1, resultList.get(resultList.size()-1).concat(array[index]));
+
+
+        String[] result = new String[resultList.size()];
+        result = resultList.toArray(result);
+
+        for(String word: result)
+            System.out.println(word);
+        System.out.println(" ");
+        return result;
     }
 
 
